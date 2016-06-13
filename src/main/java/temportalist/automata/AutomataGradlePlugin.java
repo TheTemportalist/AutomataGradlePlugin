cf5c94167fcb3538c3b20c997ea0a0c4c7075d66
TheTemportalist <the.country.gamer@gmail.com>
2016-06-12 22:54:27 -0400

    Add basics for overriding the buildscript repository for ForgeGradle

--- src/main/java/temportalist/automata/AutomataGradlePlugin.java
+++ src/main/java/temportalist/automata/AutomataGradlePlugin.java
@@ -13,8 +13,32 @@ public class AutomataGradlePlugin implements Plugin<Project> {
 	@Override
 	public void apply(final Project project) {
 
+		// START: testing extensions and tasks
+
 		project.getExtensions().add("sampleExtension", new SamplePluginExtension());
 		project.getTasks().create("sampleTask", SampleTask.class);
+
+		// END
+
+		/* Try to replace the following:
+			buildscript {
+				repositories {
+					jcenter()
+					maven {
+						name = "forge"
+						url = "http://files.minecraftforge.net/maven"
+					}
+				}
+				dependencies {
+					classpath 'net.minecraftforge.gradle:ForgeGradle:2.2-SNAPSHOT'
+				}
+			}
+
+			apply plugin: 'net.minecraftforge.gradle.forge'
+		 */
+		project.getBuildscript().getRepositories(); // add
+		project.getBuildscript().getDependencies().add("classpath", "net.minecraftforge.gradle:ForgeGradle:2.2-SNAPSHOT");
+		project.getPlugins().apply("net.minecraftforge.gradle.forge");
 
 	}
 
