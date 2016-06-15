911baa094a6a5eb8ca7a0bf0ca2fcae274441038
TheTemportalist <the.country.gamer@gmail.com>
2016-06-15 14:59:12 -0400

    Patch (temporary) for developer roles

--- src/main/groovy/temportalist/automata/Archives.groovy
+++ src/main/groovy/temportalist/automata/Archives.groovy
@@ -58,7 +58,7 @@ class Archives {
 								id = developerMap.get(EnumDeveloperStat.ID)
 								name = developerMap.get(EnumDeveloperStat.NAME)
 								roles {
-									role = developerMap.get(EnumDeveloperStat.ROLE).toString()
+									role "developer"
 								}
 							}
 						}
