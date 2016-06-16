260af28cbfb418744f2b4058ded500fe0955603c
TheTemportalist <the.country.gamer@gmail.com>
2016-06-16 13:44:50 -0400

    Add NPE and empty checks on maps for dependencies

--- src/main/groovy/temportalist/automata/PropertyDependencies.groovy
+++ src/main/groovy/temportalist/automata/PropertyDependencies.groovy
@@ -16,32 +16,36 @@ class PropertyDependencies {
 	def load(Project project, ForgeExtension minecraft) {
 		project.getRepositories().mavenCentral()
 		project.getRepositories().jcenter()
-		project.repositories {
-			for (Map.Entry<String, String> entry : this.repositories) {
-				maven {
-					name = entry.getKey()
-					url = entry.getValue()
+		if (this.repositories != null && !this.repositories.isEmpty()) {
+			project.repositories {
+				for (Map.Entry<String, String> entry : this.repositories) {
+					maven {
+						name = entry.getKey()
+						url = entry.getValue()
+					}
 				}
 			}
 		}
-		for (Map<String, Object> dependency : this.dependencies) {
-			project.dependencies.add(
-					dependency.get("compileWith"),
-					String.format("%s:%s:%s",
-							dependency.get("group"), dependency.get("name"), dependency.get("version")
-					)
-			)
-			if (dependency.hasProperty("replace")) {
-				def replaceClosure = dependency.get("replace") as Closure<?>
-				def original = String.format("%s:%s",
-						replaceClosure.getProperty("instruction"),
-						replaceClosure.getProperty("modid")
-				)
-				minecraft.replace(original,
-						String.format("%s@%s", original,
-								replaceClosure.getProperty("versionRange")
+		if (this.dependencies != null && !this.dependencies.isEmpty()) {
+			for (Map<String, Object> dependency : this.dependencies) {
+				project.dependencies.add(
+						dependency.get("compileWith"),
+						String.format("%s:%s:%s",
+								dependency.get("group"), dependency.get("name"), dependency.get("version")
 						)
 				)
+				if (dependency.hasProperty("replace")) {
+					def replaceClosure = dependency.get("replace") as Closure<?>
+					def original = String.format("%s:%s",
+							replaceClosure.getProperty("instruction"),
+							replaceClosure.getProperty("modid")
+					)
+					minecraft.replace(original,
+							String.format("%s@%s", original,
+									replaceClosure.getProperty("versionRange")
+							)
+					)
+				}
 			}
 		}
 	}
